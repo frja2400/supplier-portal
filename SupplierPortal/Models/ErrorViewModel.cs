f6b2a9a3828df2cbca809795e9e7e79e54bce92e
Frida Jansson <fridahannajansson@gmail.com>
2026-07-21 12:00:36 +0200

    first commit

---
+++ SupplierPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SupplierPortal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
