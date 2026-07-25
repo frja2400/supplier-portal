4597a2171dcbab092b5202408a9c420e02e571e6
Frida Jansson <fridahannajansson@gmail.com>
2026-07-25 19:21:10 +0200

    Add public home page with activation and supplier statistics
    - HomeController.Index queries counts via EF Core CountAsync
    - HomeStatsViewModel added for the stats display

--- SupplierPortal/Controllers/HomeController.cs
+++ SupplierPortal/Controllers/HomeController.cs
@@ -1,24 +1,43 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SupplierPortal.Data;
 using SupplierPortal.Models;
+using SupplierPortal.Models.ViewModels;
 
-namespace SupplierPortal.Controllers;
-
-public class HomeController : Controller
+namespace SupplierPortal.Controllers
 {
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-        return View();
-    }
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public async Task<IActionResult> Index()
+        {
+            var stats = new HomeStatsViewModel
+            {
+                ActivationCount = await _context.Activations.CountAsync(),
+                SupplierCount = await _context.Suppliers.CountAsync()
+            };
+
+            return View(stats);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
---
+++ SupplierPortal/Models/ViewModels/HomeStatsViewModel.cs
@@ -0,0 +1,8 @@
+namespace SupplierPortal.Models.ViewModels
+{
+    public class HomeStatsViewModel
+    {
+        public int ActivationCount { get; set; }
+        public int SupplierCount { get; set; }
+    }
+}
